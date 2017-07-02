1944d4337805cc934c9423c7744a6c4367af7461
Matt Durr <matt@onedurr.com>
2017-07-02 16:57:51 -0600

    add package name

--- package.js
+++ package.js
@@ -1,4 +1,5 @@
 Package.describe({
+    name: 'matadur:meteor-bootstrap3-datetimepicker',
     summary: "Bootstrap 3 DateTime picker from @Eonasdan, packaged for Meteor.js",
     version: "4.18.0",
     git: "https://github.com/matadur/meteor-bootstrap3-datetimepicker"
